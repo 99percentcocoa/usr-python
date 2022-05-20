69a7d74bc09c31a1dde17bff88e981ab203fc166
Saarang Rajguru <saarang.rajguru@protonmail.ch>
2022-05-20 16:49:29 +0530

    initial

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 from isc_parser import Parser
 from wxconv import WXC
 import requests, json
-import threading
+# import threading
 
 # setting up debug
 logging.basicConfig(level=logging.DEBUG)
